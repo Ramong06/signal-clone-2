8daeeba3b819176be7afb3d983eaf15cd0d4ae38
RG-Mac <ramong5406@gmail.com>
2021-04-21 15:31:49 -0500

    added firebase.js config info

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6ByfYOBUL3ozxL46wbQfrbzwuGWJ-pv0",
+  authDomain: "signal-clone-2-c73fc.firebaseapp.com",
+  projectId: "signal-clone-2-c73fc",
+  storageBucket: "signal-clone-2-c73fc.appspot.com",
+  messagingSenderId: "703030466188",
+  appId: "1:703030466188:web:ce3bbd03104e6b74738da9"
+};
