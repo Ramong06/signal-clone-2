a17e84ce5622d19ced7a63c7bded958dc2cb4b29
RG-Mac <ramong5406@gmail.com>
2021-04-22 15:45:36 -0500

    added StatusBar to Login Screen and styled

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
     return (
         <View>
+            <StatusBar style="light" />
             <Text>This is the Login Screen</Text>
         </View>
     )
