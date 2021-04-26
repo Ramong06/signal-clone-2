7a844be48704f29772eea33ad0c2d5dbf0a307be
RG-Mac <ramong5406@gmail.com>
2021-04-26 15:56:34 -0500

    added input field for register screen using State

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'react-native';
 import { KeyboardAvoidingView } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Input } from "react-native-elements";
+
 
 const RegisterScreen = ({ navigation }) => {
+    const [name, setName] = useState("");
+
     return (
         <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <StatusBar style="light" />
-            <Text>I Am THE REGISTER SCREEN!</Text>
+            <Text h3 style={{ marginBottom: 50 }}>
+                Create A Signal Account
+            </Text>
+
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder="Full Name"
+                    autofocus
+                    type="text"
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                />
+            </View>
         </KeyboardAvoidingView>
     )
 }
