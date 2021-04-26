6d03ff08a47fab20d585ab0cbdcabbecece19066
RG-Mac <ramong5406@gmail.com>
2021-04-26 15:39:27 -0500

    added StatusBar to Register Screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const RegisterScreen = ({ navigation }) => {
     return (
-        <View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
             <Text>I Am THE REGISTER SCREEN!</Text>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
