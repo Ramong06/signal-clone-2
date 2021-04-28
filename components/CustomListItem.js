06aaf67fbbdcabe9d44cf57d946cea2a9d9d059d
RG-Mac <ramong5406@gmail.com>
2021-04-28 17:27:13 -0500

    added list item and avatar to custom list items.js

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ListItem, Avatar } from "react-native-elements";
 
 const CustomListItem = () => {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <ListItem>
+            <Avatar
+                rounded
+                source={{
+                    uri:
+                        chatMessages?.[0]?.photoURL ||
+                        "https://cdn.shopify.com/s/files/1/0030/4227/9494/products/mgs-solid-snake-tubbz-gs-04_800x.progressive.jpg?v=1611082441"
+                }}
+            />
+        </ListItem>
     )
 }
 
